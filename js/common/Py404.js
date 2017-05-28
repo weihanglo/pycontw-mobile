afbe5e5203d39c339231147c83231e6edaad54f4
Weihang Lo <weihanglotw@gmail.com>
2017-05-29 05:58:52 +0800

    feat: `Py404` new custom error page

---
+++ js/common/Py404.js
@@ -0,0 +1,72 @@
+// This presentational + container component
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  StyleSheet,
+  View,
+  ViewPropTypes
+} from 'react-native'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import {NavigationActions} from 'react-navigation'
+
+import * as Colors from './PyColors'
+import {Heading1} from './PyText'
+import Header from './PyHeader'
+
+Py404.propTypes = {
+  title: PropTypes.string,
+  titleColor: PropTypes.string,
+  headerColor: PropTypes.string,
+  mainContent: PropTypes.node,
+  goBack: PropTypes.func,
+  style: ViewPropTypes.style
+}
+
+function Py404 ({
+  title = 'Error',
+  titleColor = Colors.LIGHT_TEXT,
+  headerColor = Colors.primary.DARK_BLUE,
+  mainContent = 'Oops!\n\nNot Found :(',
+  goBack,
+  style,
+  ...props
+}) {
+  let content = mainContent
+  if (typeof content === 'string') {
+    content = <Heading1 style={{textAlign: 'center'}}>{mainContent}</Heading1>
+  }
+  const leftItem = <Header.BackButton onPress={goBack} color={titleColor} />
+  return (
+    <View style={[styles.container, style]}>
+      <Header
+        titleColor={titleColor}
+        leftItem={leftItem}
+        centerItem={title}
+        style={{backgroundColor: headerColor}}
+      />
+      <View style={styles.inner}>
+        {content}
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  inner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+// react-redux part --------------------
+
+const mapDispatchToProps = dispatch => ({
+  goBack: () => { dispatch(NavigationActions.back()) }
+})
+
+export default connect(null, mapDispatchToProps)(Py404)
